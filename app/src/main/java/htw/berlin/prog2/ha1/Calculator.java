c5e86a7d8824d64ee20cb9f056ec019844d1dd01
JakobScharf1 <jakob.scharf@gmx.net>
2022-10-10 17:17:29 +0200

    Teilaufgabe 3.1 - Korrektur Methode pressUnaryOperationKey

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -74,13 +74,15 @@ public class Calculator {
     public void pressUnaryOperationKey(String operation) {
         latestValue = Double.parseDouble(screen);
         latestOperation = operation;
-        var result = switch(operation) {
-            case "√" -> Math.sqrt(Double.parseDouble(screen));
-            case "%" -> Double.parseDouble(screen) / 100;
-            case "1/x" -> 1 / Double.parseDouble(screen);
-            default -> throw new IllegalArgumentException();
-        };
-        screen = Double.toString(result);
+        if(latestValue != 0){
+            var result = switch(operation) {
+                case "√" -> Math.sqrt(Double.parseDouble(screen));
+                case "%" -> Double.parseDouble(screen) / 100;
+                case "1/x" -> 1 / Double.parseDouble(screen);
+                default -> throw new IllegalArgumentException();
+            };
+            screen = Double.toString(result);
+        }
         if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
 
     }
