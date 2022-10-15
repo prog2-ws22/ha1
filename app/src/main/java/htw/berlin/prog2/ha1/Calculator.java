b9470e82bd935bf502602b8283c467a8713e279c
fitimr11 <fitimr11@gmail.com.com>
2022-10-15 19:18:22 +0200

    1. roter Test Fix

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -126,6 +126,7 @@ public class Calculator {
         };
         screen = Double.toString(result);
         if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
-        if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
+        //if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
+        if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 5);
     }
 }
