c918fea61970719b8c8d10c69122de9aa40548d8
JWLaessig <s0573711@htw-berlin.de>
2022-10-17 15:23:48 +0200

    1. grüner Test

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -41,5 +41,20 @@ class CalculatorTest {
     }
 
     //TODO hier weitere Tests erstellen
+    @Test
+    @DisplayName("should display result after multiply two positive numbers")
+    void testPositiveMultiplication() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(2);
+        calc.pressBinaryOperationKey("x");
+        calc.pressDigitKey(3);
+        calc.pressEqualsKey();
+
+        String expected = "6";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
 }
 
