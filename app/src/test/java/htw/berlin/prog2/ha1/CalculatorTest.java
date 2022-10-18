00e74ae75b8c340a46a5dd435e7d294031113cd1
janishasithmi <jenishereeza@gmail.com>
2022-10-18 16:19:44 +0200

    test

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -14,13 +14,13 @@ class CalculatorTest {
         Calculator calc = new Calculator();
 
         calc.pressDigitKey(2);
-        calc.pressDigitKey(0);
+        calc.pressDigitKey(5);
         calc.pressBinaryOperationKey("+");
         calc.pressDigitKey(2);
         calc.pressDigitKey(0);
         calc.pressEqualsKey();
 
-        String expected = "40";
+        String expected = "45";
         String actual = calc.readScreen();
 
         assertEquals(expected, actual);
@@ -42,10 +42,61 @@ class CalculatorTest {
     }
 
     //TODO hier weitere Tests erstellen
+    @Test
+    @DisplayName("Test")
+    void testPrecentage(){
 
+        Calculator calc = new Calculator();
 
+        calc.pressDigitKey(2);
+        calc.pressDigitKey(5);
+        calc.pressDigitKey(0);
+        calc.pressUnaryOperationKey("%");
 
+        String expected = "2.5";
+        String actual = calc.readScreen();
 
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    @DisplayName("Test")
+    void testabstraction(){
+
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(2);
+        calc.pressDigitKey(5);
+
+        calc.pressUnaryOperationKey("/");
+        calc.pressDigitKey(0);
+        calc.pressEqualsKey();
+
+        String expected = "Error";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    @DisplayName("Test")
+    void testabstractionnegative(){
+
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(-2);
+        calc.pressUnaryOperationKey("/");
+        calc.pressDigitKey(2);
+        calc.pressEqualsKey();
+
+        String expected = "-1";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+
+    }
 
 
 
