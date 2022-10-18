abc05705e38c2052442fb2987cd8042f7bff73dc
Mohanad <Mohanad.Dakkak@Student.HTW-Berlin.de>
2022-10-18 15:02:51 +0200

    test

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+// test
 @DisplayName("Retro calculator")
 class CalculatorTest {
 
