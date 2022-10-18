25eef8e7fd8d13ab4ae8d27637aaa95453a22284
JLkn <107927104+JLkn@users.noreply.github.com>
2022-10-18 17:56:24 +0200

    Fix für Test zum multiplizieren von negativen Zahlen

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -60,6 +60,10 @@ public class Calculator {
      * @param operation "+" für Addition, "-" für Substraktion, "x" für Multiplikation, "/" für Division
      */
     public void pressBinaryOperationKey(String operation)  {
+        if ( operation == "-" && screen == "0"){
+            pressNegativeKey();
+            return;
+        }
         latestValue = Double.parseDouble(screen);
         latestOperation = operation;
     }
