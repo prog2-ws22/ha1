14e5906af83ba060b3b4bb02c6703ee984f75b4e
Daria <noyyt@mail.ru>
2022-10-20 21:46:17 +0200

    Commit für den Fix zum 1. roten Test

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -83,6 +83,7 @@ public class Calculator {
         };
         screen = Double.toString(result);
         if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
+        if(screen.equals("0.0")) screen = "0";
 
     }
 
