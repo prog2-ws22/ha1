4d3660ab90768487148c5448188d5bd6e0e0be78
s0583659 <emre.sag@student.htw-berlin.de>
2022-10-25 01:08:29 +0200

    5. Commit für Fix zu diesem Test.

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -82,7 +82,7 @@ public class Calculator {
         };
         screen = Double.toString(result);
         if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
-
+        if(screen.contains("NaN")) screen = ("Error"); // In Aufgabe 3. Fehler bei der Rückgabe = NaN
     }
 
     /**
