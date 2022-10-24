7fc5c2e842023b8f142490602e81a1ea4c2203e2
douniabouzidi <s0577579@htw-berlin.de>
2022-10-24 17:32:35 +0200

    3. Commit für den Fix zu diesem Test

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -93,7 +93,8 @@ public class Calculator {
      * Beim zweimaligem Drücken, oder wenn bereits ein Trennzeichen angezeigt wird, passiert nichts.
      */
     public void pressDotKey() {
-        if(!screen.endsWith(".")) screen = screen + ".";
+        if(!screen.contains("."))
+            screen = screen + ".";
     }
 
     /**
