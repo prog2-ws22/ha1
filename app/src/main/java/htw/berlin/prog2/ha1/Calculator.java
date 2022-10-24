c939d553bd9a90c6460265e90ddc4630b408cc9b
douniabouzidi <s0577579@htw-berlin.de>
2022-10-24 17:35:46 +0200

    5. Commit für Fix zu diesem Test

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -128,5 +128,6 @@ public class Calculator {
         screen = Double.toString(result);
         if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
         if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
+        if(Double.isInfinite(result)) screen = "ERROR";
     }
 }
