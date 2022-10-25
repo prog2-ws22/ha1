080700b91d6b2e859d9491be27c4fa0cf7faa3dd
Deniz <s0571123@htw-berlin.de>
2022-10-25 22:47:14 +0200

    1st red Commit fixed

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -125,7 +125,9 @@ public class Calculator {
             default -> throw new IllegalArgumentException();
         };
         screen = Double.toString(result);
+        if(screen.contains("Infinity")) screen = "ERROR";
         if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
         if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
+
     }
 }
