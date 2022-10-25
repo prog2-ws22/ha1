e75dfb9499f7ee5aa55cdddf21a8e0a04e8ee6d1
DavidNgo <David.Ngo@Student.HTW-Berlin.de>
2022-10-25 20:08:41 +0200

    Alle 3 fertig erstellten Testfälle (Test 1 richtig,Test 2 Fehler gefunden und korrigiert, Test 3 Fehler gefunden)

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -127,8 +127,8 @@ public class Calculator {
         screen = Double.toString(result);
         if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
         if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
-        //if(screen == "Infinity") {
-        //    screen = "Error";
-        //}
+        if(screen == "Infinity") {
+            screen = "Error";
+        }
     }
 }
