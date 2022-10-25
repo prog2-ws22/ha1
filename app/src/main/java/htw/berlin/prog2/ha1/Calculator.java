1f370c12e6ef00425cc7698dc84f0270e3a7b49d
Islamovaa <Liana.Islamova@student.htw-berlin.de>
2022-10-25 13:43:42 +0200

    Teilaufgabe 3: gefixter Bug bei Division mit 0

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -96,6 +96,7 @@ public class Calculator {
      * Beim zweimaligem Drücken, oder wenn bereits ein Trennzeichen angezeigt wird, passiert nichts.
      */
     public void pressDotKey(){
+
         if(!screen.endsWith(".")) screen = screen + ".";
     }
 
