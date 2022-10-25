bc737626b20648409cd2cbbe1d623097218f3dcf
Max Gurgel <s0569230@htw-berlin.de>
2022-10-25 18:59:36 +0200

    fixing 1st red Test

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -117,15 +117,24 @@ public class Calculator {
      * und das Ergebnis direkt angezeigt.
      */
     public void pressEqualsKey() {
-        var result = switch(latestOperation) {
-            case "+" -> latestValue + Double.parseDouble(screen);
-            case "-" -> latestValue - Double.parseDouble(screen);
-            case "x" -> latestValue * Double.parseDouble(screen);
-            case "/" -> latestValue / Double.parseDouble(screen);
+        String result = switch(latestOperation) {
+            case "+" -> Double.toString(latestValue + Double.parseDouble(screen));
+            case "-" -> Double.toString(latestValue - Double.parseDouble(screen));
+            case "x" -> Double.toString(latestValue * Double.parseDouble(screen));
+            case "/" -> divide(latestValue);
             default -> throw new IllegalArgumentException();
         };
-        screen = Double.toString(result);
+        screen = result;
         if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
         if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
+    }
+
+    public String divide(double latestValue) {
+        if(Double.parseDouble(screen)==0) {
+            return "Error";
+        }
+        else {
+            return Double.toString(latestValue / Double.parseDouble(screen));
+        }
     }
 }
